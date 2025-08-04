47c37ee4571773d0f3712ba7e144122d9bbba054
RADS12 <52092714+RADS12@users.noreply.github.com>
2025-08-04 15:35:21 -0400

    Added Database manager and related code

--- RegulatoryCompliance/Program.cs
+++ RegulatoryCompliance/Program.cs
@@ -3,6 +3,9 @@ using Common.Helpers;
 using Common.Interfaces;
 using RuleEngine.Facade;
 using RuleEngine.Interfaces;
+using SalManager;
+using SqlManager;
+using SqlManager.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.Configure<AppSettingsConfig>(builder.Configuration.GetSection("AppSettings"));
 builder.Services.AddSingleton<IAppSettingsService, AppSettingsService>();
+builder.Services.AddSingleton<IDbConectionFactory, SqlDbConnectionFactory>();
 
 builder.Services.AddTransient<IRegulatoryRuleFacade, SafeHarborRuleFacade>();
 builder.Services.AddTransient<IRegulatoryRuleFacade, HighCostRuleFacade>();
@@ -20,7 +24,6 @@ builder.Services.AddTransient<IRegulatoryRuleFacade, StateRegulatoryRulesFacade>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 var app = builder.Build();
 
