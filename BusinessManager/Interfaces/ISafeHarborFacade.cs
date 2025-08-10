80a8b7e137ee6b2f961f86654a4662cd12dda819
RADS12 <52092714+RADS12@users.noreply.github.com>
2025-08-10 16:39:44 -0400

    Fixed tests

---
+++ BusinessManager/Interfaces/ISafeHarborFacade.cs
@@ -0,0 +1,14 @@
+﻿using Common.Configuration;
+using Common.Enums;
+using Common.Models;
+
+namespace RuleEngine.Interfaces
+{
+    public interface ISafeHarborFacade : IRegulatoryRulesEngine
+    {
+        RegulatoryTestType RuleType { get; }
+        //gulatoryTestResult RunTest(RegulatoryTestInput input, AppSettingsConfig config);
+
+        SafeHarborTestResults RunTest(RegulatoryTestInput input, AppSettingsConfig config);
+    }
+}
